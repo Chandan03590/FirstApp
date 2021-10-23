271b9e3473893741cf2fc48c5dcdbe330de12943
chandan <akc960857@gmail.com>
2021-10-23 22:36:52 +0530

    Horizontal Linear Layout, Grid Layout and Table Layout

---
+++ app/src/main/java/com/firstappp/LoginModenUiActivity.java
@@ -0,0 +1,14 @@
+package com.firstappp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginModenUiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_moden_ui);
+    }
+}
